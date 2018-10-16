47ea9c23adeea952285734f9e1dcc40f80c9898d
nanc <nanc@adobe.com>
2018-10-17 00:10:24 +0530

    AppBarLayout with tabLayout

--- app/src/main/java/com/nancy/whatsappui/MainActivity.java
+++ app/src/main/java/com/nancy/whatsappui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.nancy.whatsappui;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Display;
 import android.widget.LinearLayout;
 
@@ -22,7 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recycle = findViewById(R.id.recycler);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+
+        TabLayout tabLayout=(TabLayout)findViewById(R.id.tabs);
+
+        tabLayout.addTab(tabLayout.newTab().setText("CHATS"));
+        tabLayout.addTab(tabLayout.newTab().setText("STATUS"));
+        tabLayout.addTab(tabLayout.newTab().setText("CALLS"));
         LinearLayoutManager lay=new LinearLayoutManager(this);
         recycle.setLayoutManager(lay);
 
@@ -31,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         recycle.addItemDecoration(decoration);
 
         List<ModelClass> listModelCopy  = setData("Nancy","12:00","Hi");
+        //listModelCopy = setData("Adi","13:00","Bye");
 
         adapter = new RecyclerViewAdapter(this,listModelCopy);
         recycle.setAdapter(adapter);
